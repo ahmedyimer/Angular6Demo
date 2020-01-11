db9c1747badd1cfcaa874e0feaa154d74f4fca13
Ahmed Yimer <ahmed.yimer@gmail.com>
2020-01-11 13:28:54 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Import the components so they can be referenced in routes
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
 
+
+// The last route is the empty path route. This specifies
+// the route to redirect to if the client side path is empty.
+const routes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
+
+// Pass the configured routes to the forRoot() method
+// to let the angular router know about our routes
+// Export the imported RouterModule so router directives
+// are available to the module that imports this AppRoutingModule
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+// Import FormGroup and FormControl classes
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styles: []
+  //styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  // This FormGroup contains fullName and Email form controls
+  employeeForm: FormGroup;
+
+  constructor() { }
+
+  // Initialise the FormGroup with the 2 FormControls we need.
+  // ngOnInit ensures the FormGroup and it's form controls are
+  // created when the component is initialised
+  ngOnInit() {
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl(),
+      //Create skills form group
+      skills: new FormGroup({
+          SkillName: new FormControl(),
+          experienceInYears: new FormControl(),
+          proficiency: new FormControl()
+        })
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.employeeForm.value);
+    console.log(this.employeeForm);
+  }
+  
+
+
+
+}
